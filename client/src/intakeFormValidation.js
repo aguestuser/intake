4da4afd28f287ee2c07d252e290a96a674779ff4
Thiago de Mello Bueno <thiago@madeofpeople.org>
2017-03-09 21:03:42 -0500

    removed debugging code

--- client/src/intakeFormValidation.js
+++ client/src/intakeFormValidation.js
@@ -18,7 +18,7 @@ const validateRequiredFields = (values, requiredFields) => (errors) =>
   requiredFields.reduce((_errors, field) =>
       values[field]
         ? _errors
-        : ({ ..._errors, [field]: '<div class="!!!">Required</div>' })
+        : ({ ..._errors, [field]: 'Required' })
     , errors)
 
 const validateEmail = (values, field) => (errors) =>
