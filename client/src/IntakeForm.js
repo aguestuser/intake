232f5f2e15ca441b92603409621f356e3d320787
Thiago de Mello Bueno <thiago@madeofpeople.org>
2017-03-09 21:11:32 -0500

    remove spaces

--- client/src/IntakeForm.js
+++ client/src/IntakeForm.js
@@ -12,15 +12,10 @@ import {SOURCE_CODE_LINK} from './constants'
 const IntakeForm = props => {
   const { handleSubmit, pristine, submitting, reset } = props
   return (
-
     <div className="intake-form">
-
       <AppBar className="app-bar" title="Intake Form" />
-
       <Card className="form-wrapper">
         <form onSubmit={data => handleSubmit(data).then(reset)}>
-
-
           <section className="form-group">
             <div className="field-group">
               <h2 className="group-title">Volunteer Information</h2>
@@ -28,17 +23,13 @@ const IntakeForm = props => {
               <Field name="volunteerEmail" component={MuiTextField} label="Volunteer Email"/>
             </div>
           </section >
-
           <section className="form-group">
-
             <h2 className="group-title">Client Information</h2>
-
             <div className="field-group">
               <Field name="clientName" component={MuiTextField} label="Client Name"/>
               <Field name="clientEmail" component={MuiTextField} label="Client Email"/>
               <Field name="clientPhone" component={MuiTextField} label="Client Phone"/>
             </div>
-
             <div className="field-group">
               <h3 className="field-title">Client Country of Origin:</h3>
               {/*TODO(ag|2.22.17): these should actually be checkboxes, and should be `clientCitizenship`*/}
@@ -56,7 +47,6 @@ const IntakeForm = props => {
               {/*TODO(ag|2.22.17): this should be a date picker*/}
               <Field name="travelDate" component={MuiTextField} label="Date of Travel"/>
             </div>
-
             <div className="field-group">
               <h3 className="field-title">Client Visa Type:</h3>
               <Field name="clientVisaType" component={MuiRadioGroup}>
@@ -68,9 +58,7 @@ const IntakeForm = props => {
                 <RadioButton value="special" label="Special Immigrant Visa"/>
               </Field>
             </div>
-
           </section>
-
           <section className="form-actions">
             <RaisedButton
               type="submit"
@@ -79,15 +67,12 @@ const IntakeForm = props => {
               primary
             />
           </section>
-
         </form>
-
         <footer className="page-footer">
           <Subheader>
              <a href={SOURCE_CODE_LINK}>Source Code</a>
           </Subheader>
         </footer>
-
       </Card>
     </div>
   )
